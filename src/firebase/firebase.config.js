14788db3912dffdcf332feca8dc8260cd42b4818
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-10-21 11:51:08 +0600

    basic project setup (react, react router, tailwind, firebase))

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAj1ua4EPCLzU11_VC5Ol8LWrZgL149n7U",
+  authDomain: "online-news-portal-a110a.firebaseapp.com",
+  projectId: "online-news-portal-a110a",
+  storageBucket: "online-news-portal-a110a.appspot.com",
+  messagingSenderId: "440628864174",
+  appId: "1:440628864174:web:f52f38528594031112f93f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
